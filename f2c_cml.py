444495b2dfe3718a2dbd99b5885f6abcbca43b09
hi-lary <beansy7@sbcglobal.net>
2023-02-28 18:11:53 -0600

    homework

--- f2c_cml.py
+++ f2c_cml.py
@@ -1,1 +1,6 @@
 # exercise 4.2
+"""exercise 4.1 but fahrenheit is read from the command line"""
+f2c_cml.py 87
+import sys
+print(float(sys.argv[1]))
+
--- f2c_qa.py
+++ f2c_qa.py
@@ -1,1 +1,10 @@
 # exercise 4.1
+f = eval(input('temperature fahrenheit:'))
+
+
+def celsius(fahrenheit):
+    celsius = (5 / 9) * (fahrenheit - 32)
+    return celsius
+
+
+print(f'{f} degrees fahrenheit is equivalent to {celsius(f):0.3} degrees in celsius.')
